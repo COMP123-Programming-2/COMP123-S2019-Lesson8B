f25d5366d90c9bc1ea04a20f16459cbd4f908c7b
301034220 <301034220@PR-E216-52>
2019-06-26 15:42:42 -0400

    completed initial user interface

--- COMP123-S2019-Lesson8B/Lab08Form.cs
+++ COMP123-S2019-Lesson8B/Lab08Form.cs
@@ -16,5 +16,15 @@ namespace COMP123_S2019_Lesson8B
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
